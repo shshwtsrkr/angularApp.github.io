71976dd24ed960b61626c9f9771e6393497e3702
sas <ok@gmail.com>
2022-09-20 10:34:23 +0530

    Modified To Do List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  /*title = 'my-app';
   count = -1;
   lon = [1, 2, 3, 4, 5, 6, 7];
   names = ["sash", "bash", "zash"];
+  
   increaseCount = () => {
     ++this.count;
   }
 
   decreaseCount = () => {
     --this.count;
+  }
+  */
+  tdi: string = '';
+  lots: string[] = [];
+  
+  addToDoList = () => {
+    this.lots.push(this.tdi)
   }
 }
 
